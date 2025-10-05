bc86d02d9211dc93c2e0d7ad5d34a9fc72c0abad
VishalChauhan87 <vc9196010@gmail.com>
2025-10-05 13:30:21 +0530

    Initial commit

---
+++ 02_problem.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+ {
+ // int length = 3;
+ //   int breath = 6;
+ int length ,breath;
+ printf("Enter length\n");
+ scanf("%d", &length);
+
+ printf("Enter breath\n");
+ scanf("%d", &breath);
+
+     printf("The area of rectangle is %d", length*breath);
+    return 0;
+}
---
+++ chapter2/02_problem.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    int a= 3*2/3-3+1;
+    printf("The value of a is %d", 3*2/3-3+1);
+    return 0;
+}
